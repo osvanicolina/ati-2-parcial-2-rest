1b4623d7d5fe8430eb051470bea97f8b220fd1a1
Diego Sánchez <diegosandmg@gmail.com>
2022-08-20 17:15:59 -0400

    heroku

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python manage.py migrate --noinput --settings django_src.settings.heroku
-python manage.py loaddata  --settings django_src.settings.heroku fixtures/admin.json
+python manage.py loaddata fixtures/admin.json
