c2c458f64a16d02a1070f5eb6d1a6f9ce5804099
Diego Sánchez <diegosandmg@gmail.com>
2022-08-20 17:30:08 -0400

    heroku

--- django_src/wsgi.py
+++ django_src/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_src.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_src.settings.heroku')
 
 application = get_wsgi_application()
--- release-tasks.sh
+++ release-tasks.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo "READY"
+python manage.py migrate --noinput --settings django_src.settings.heroku
+python manage.py loaddata  --settings django_src.settings.heroku fixtures/admin.json
